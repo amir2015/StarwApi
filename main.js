101867bca7cf5ae81ad75cc332c621b4a07575ff
amir2015 <zahranovich2@gmail.com>
2023-02-09 20:54:50 +0200

    mum of entities added

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const results = document.querySelector("#results");
+const numberOfElements = document.querySelector("#numberOfElements");
 const btnNext = document.querySelector("#btnNext");
 const btnPrev = document.querySelector("#btnPrev");
 const navigationButton = document.querySelector("#navigationButtons");
 navigationButton.style.visibility = "hidden";
+const btnShowFilms = document.querySelector("#btnShowFilms");
 
 const asyncFetch = async (value) => {
   var res = await fetch(`https://swapi.dev/api/${value}`).catch((err) => {
@@ -43,10 +45,17 @@ const asyncFetch = async (value) => {
     throw new Error(res.statusText);
   }
 };
-
+const getMyFilms = () => {
+  console.log("myFilms");
+};
 const resultShow = async (data, value) => {
   let output = "";
+  let unmberOutput = "";
   if (value === "people") {
+    console.log(data.count, "first people ");
+    unmberOutput += `
+<span class="btn btn-md btn-outline-dark" ><h3>Number Of ${value} is ${data.count} </h3></span>
+`;
     data.results.forEach((item) => {
       output += `<div class="card p-3 m-3" style="opacity:1">
                 <div class="card-body">
@@ -57,6 +66,33 @@ const resultShow = async (data, value) => {
                   <li class="list-group-item">Birth year: ${item.birth_year} </li>
                   <li class="list-group-item">Hair colour: ${item.hair_color}</li>
                   <li class="list-group-item">Mass: ${item.mass}</li>
+                  <li class="list-group-item">Number of Films: ${item.films.length}</li>
+                  <li class="list-group-item">Films: 
+                  <button type="button" 
+                  id="triggerModal"
+                
+                  class="btn btn-md btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  show more
+                  </button>
+                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                  <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+         <button type="button" class="btn btn-outline-dark" data-bs-dismiss ="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+                  </li>
+                  
                 </ul>
                 
                 <div class="card-body">
@@ -66,7 +102,11 @@ const resultShow = async (data, value) => {
   `;
     });
   }
+
   if (value === "starships") {
+    unmberOutput += `
+    <span class="btn btn-md btn-outline-dark" ><h3>Number Of ${value} is ${data.count} </h3></span>
+    `;
     data.results.forEach((item) => {
       output += `<div class="card p-3 m-3" style="opacity:1">
                 <div class="card-body">
@@ -86,6 +126,9 @@ const resultShow = async (data, value) => {
   }
 
   if (value === "films") {
+    unmberOutput += `
+    <span class="btn btn-md btn-outline-dark" ><h3>Number Of ${value} is ${data.count} </h3></span>
+    `;
     data.results.forEach((item) => {
       output += `<div class="card p-3 m-3" style="opacity:1">
                 <div class="card-body">
@@ -104,6 +147,7 @@ const resultShow = async (data, value) => {
     });
   }
   results.innerHTML = output;
+  numberOfElements.innerHTML = unmberOutput;
 };
 
 document.querySelector("#buttons").addEventListener("click", (e) => {
